c9c76487f9c2167ba051f616160cb108f23cb7f4
Grishma Ptael <patelgrishma9911@gmail.com>
2019-12-10 21:56:42 -0500

    files

---
+++ src/AppFooter.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './AppFooter.css';
+
+class AppFooter extends Component {
+  render() {
+    return (
+      <div className="App-footer">
+        <h5>Copyrights @ Grishma Patel
+        </h5>
+      </div>
+    );
+  }
+}
+
+export default AppFooter;
