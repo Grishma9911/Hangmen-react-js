9fca93a763aac938d1ced4087fa99aea0751d827
Grishma Ptael <patelgrishma9911@gmail.com>
2019-12-15 22:46:14 -0500

    changes

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+// Add routes, both API and view
+app.use("/api/results", routes);
+
 // Serve up static assets (usually on heroku)
 //if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 //}
-// Add routes, both API and view
-app.use("/api/results", routes);
+
 
 // Start the API server
 app.listen(PORT, function () {
